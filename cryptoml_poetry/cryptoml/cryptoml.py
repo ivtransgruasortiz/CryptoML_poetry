f561e6b7f5aff1ba7e0b0b5bf1a99a037c319fb7
Iván Ortiz Martín <ivan@transgruasortiz.com>
2022-02-20 21:41:09 +0100

    feat: added init logo

---
+++ cryptoml_poetry/cryptoml/cryptoml.py
@@ -0,0 +1,16 @@
+
+import json
+import requests
+from keras.models import Sequential
+from keras.layers import Activation, Dense, Dropout, LSTM
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.metrics import mean_absolute_error
+
+
+
+def cryptoml():
+    print('hola')
+
---
+++ cryptoml_poetry/main.py
@@ -0,0 +1,5 @@
+from cryptoml_poetry.cryptoml.cryptoml import cryptoml
+
+# Script principal
+if __name__ == '__main__':
+    cryptoml()
